014ef638a911228a567bb69f6c89c5b292a528c0
Makoto <inouemak@googlemail.com>
2018-05-18 06:42:54 +0100

    Implement payback

--- contracts/CrowdSale.sol
+++ contracts/CrowdSale.sol
@@ -9,12 +9,24 @@ contract CrowdSale {
     address public owner;
     mapping (address => Participant) public participants;
     address[] participantsIndex;
+    Stages public stage;
+
     struct Participant{
         address buyerAddress;
         uint price;
         uint balance;
     }
 
+    enum Stages {
+        Sale,
+        Finalised,
+        Canceled
+    }
+
+    event Bought(uint deposit);
+    event Finalised();
+    event Canceled();
+    event Withdrawn(address account, uint amount);
   // Given start_price is $120, target_price is $60, capacity is 10, and threashold 
   function CrowdSale(uint _startPrice, uint _targetPrice, uint _cap, uint _threshold) public {
     require(_startPrice > _targetPrice);
@@ -27,21 +39,45 @@ contract CrowdSale {
   }
   
   function buy() public payable {
-      // TODO: Stop buying more than once
-      require(participants[msg.sender].price == 0);
-      require(!end);
-      require(participantsIndex.length < cap);
-      uint deposit = getPriceFor(participantsIndex.length + 1);
-      require(deposit == msg.value);
-      participantsIndex.push(msg.sender);
-      participants[msg.sender] = Participant(msg.sender, deposit, deposit);
+    require(participants[msg.sender].price == 0);
+    require(!end);
+    require(participantsIndex.length < cap);
+    uint deposit = getPriceFor(participantsIndex.length + 1);
+    require(deposit == msg.value);
+    participantsIndex.push(msg.sender);
+    participants[msg.sender] = Participant(msg.sender, deposit, deposit);
+    emit Bought(deposit);
   }
 
   // change send to true
   // pay back to participants;
   function finalize() public {
+    require(msg.sender == owner);
+    end = true;
+    stage = Stages.Finalised;
+    emit Finalised();
+  }
+
+  function withdraw() public{
+    require(stage != Stages.Sale);
+    Participant participant = participants[msg.sender];
+    require(participant.balance != 0);
+    uint amount;
+    if(stage == Stages.Finalised){
+        amount = participant.price - getPrice();
+    }else{
+        amount = participant.balance;
+    }
+    participant.balance = 0;
+    participant.buyerAddress.transfer(amount);
+    emit Withdrawn(participant.buyerAddress, amount);
+  }
+
+  function cancel() public {
       require(msg.sender == owner);
       end = true;
+      stage = Stages.Canceled;
+      emit Canceled();
   }
 
   function numParticipants() view returns(uint){
@@ -50,26 +86,26 @@ contract CrowdSale {
 
   // Duplicate
   function getParticipant(address participant) view returns(uint, uint){
-      return (participants[participant].price, participants[participant].balance);
+    return (participants[participant].price, participants[participant].balance);
   }
 
   function getPrice() view returns(uint){
-      return getPriceFor(participantsIndex.length);
+    return getPriceFor(participantsIndex.length);
   }
 
   function getPriceFor(uint num) public view returns(uint){
-       if (participantsIndex.length == 0){
-           return startPrice;
-       }
-       if (participantsIndex.length < threshold){
-           return startPrice;
-       }
-       if (participantsIndex.length == cap){
-           return targetPrice;
-       }
-       uint delta = (startPrice - targetPrice) / (cap - threshold);
-       uint bonusDays = participantsIndex.length - threshold;
-       uint result = startPrice - (delta * (bonusDays +1));
-       return result;
+    if (participantsIndex.length == 0){
+        return startPrice;
+    }
+    if (participantsIndex.length < threshold){
+        return startPrice;
+    }
+    if (participantsIndex.length == cap){
+        return targetPrice;
+    }
+    uint delta = (startPrice - targetPrice) / (cap - threshold);
+    uint bonusDays = participantsIndex.length - threshold;
+    uint result = startPrice - (delta * (bonusDays +1));
+    return result;
   }
 }
--- test/crowdsale.js
+++ test/crowdsale.js
@@ -1,20 +1,8 @@
 var CrowdSale = artifacts.require("./CrowdSale.sol");
 
 contract('CrowdSale', function(accounts) {
-  let sale;
   let owner = accounts[0];
-  beforeEach(async function(){
-    sale = await CrowdSale.new(120, 60, 10, 4, {from:owner});
-  })
-
-  it("sets config", async function() {
-    assert.equal((await sale.startPrice.call()), 120);
-    assert.equal((await sale.targetPrice.call()), 60);
-    assert.equal((await sale.cap.call()), 10);
-    assert.equal((await sale.threshold.call()), 4);
-    assert.equal((await sale.owner.call()), owner);
-    assert.equal((await sale.end.call()), false);
-  });
+  let sale;
 
   async function assertPurchase(number, before, after){
     var success = true;
@@ -33,22 +21,86 @@ contract('CrowdSale', function(accounts) {
     return success;
   }
 
-  it("reaches cap", async function() {
-    assert.equal(await assertPurchase(1, 120, 120), true);
-    assert.equal(await assertPurchase(2, 120, 120), true);
-    assert.equal(await assertPurchase(3, 120, 120), true);
-    assert.equal(await assertPurchase(4, 120, 110), true);
-    assert.equal(await assertPurchase(5, 110, 100), true);
-    assert.equal(await assertPurchase(6, 100, 90), true);
-    assert.equal(await assertPurchase(7, 90, 80), true);
-    assert.equal(await assertPurchase(8, 80, 70), true);
-    assert.equal(await assertPurchase(9, 70, 60), true);
-    assert.equal(await assertPurchase(10, 60, 60), true);
-    assert.equal(await assertPurchase(11, 60, 60), false);
-  });
+  describe('buying', function(){
+    beforeEach(async function(){
+      sale = await CrowdSale.new(120, 60, 10, 4, {from:owner});
+    })
+  
+    it("sets config", async function() {
+      assert.equal((await sale.startPrice.call()), 120);
+      assert.equal((await sale.targetPrice.call()), 60);
+      assert.equal((await sale.cap.call()), 10);
+      assert.equal((await sale.threshold.call()), 4);
+      assert.equal((await sale.owner.call()), owner);
+      assert.equal((await sale.end.call()), false);
+    });
+  
+    it("reaches cap", async function() {
+      assert.equal(await assertPurchase(1, 120, 120), true);
+      assert.equal(await assertPurchase(2, 120, 120), true);
+      assert.equal(await assertPurchase(3, 120, 120), true);
+      assert.equal(await assertPurchase(4, 120, 110), true);
+      assert.equal(await assertPurchase(5, 110, 100), true);
+      assert.equal(await assertPurchase(6, 100, 90), true);
+      assert.equal(await assertPurchase(7, 90, 80), true);
+      assert.equal(await assertPurchase(8, 80, 70), true);
+      assert.equal(await assertPurchase(9, 70, 60), true);
+      assert.equal(await assertPurchase(10, 60, 60), true);
+      assert.equal(await assertPurchase(11, 60, 60), false);
+    });
+  
+    it("no duplicate purchase", async function() {
+      assert.equal(await assertPurchase(1, 120, 120), true);
+      assert.equal(await assertPurchase(1, 120, 120), false);
+    });
+  })
 
-  it("no duplicate purchase", async function() {
-    assert.equal(await assertPurchase(1, 120, 120), true);
-    assert.equal(await assertPurchase(1, 120, 120), false);
-  });
+  describe('payback', function(){
+    let startPrice = 120;
+    let beforeBalance = [];
+    let afterBalance = [];
+    beforeEach(async function(){
+      sale = await CrowdSale.new(startPrice, 100, 4, 2, {from:owner});
+      assert.equal(await assertPurchase(1, 120, 120), true);
+      assert.equal(await assertPurchase(2, 120, 110), true);
+      assert.equal(await assertPurchase(3, 110, 100), true);
+      assert.equal(await assertPurchase(4, 100, 100), true);
+    })
+
+    async function assertWithdraw(accountNumber, _, transfered){
+      const { logs } = await sale.withdraw({from:accounts[accountNumber]});
+      const event = logs.find((e) => {
+        return e.event === 'Withdrawn' && e.args.account === accounts[accountNumber]
+      });
+      assert.equal(event.args.amount.toNumber(), transfered);
+      return true;
+    }
+
+    it("finalises", async function() {
+      await sale.finalize({from:owner});
+      assert.equal((await sale.end.call()), true);
+      assert.equal((await sale.stage.call()), 1);
+      assert.equal(await assertWithdraw(1, 120, 20), true);
+      assert.equal(await assertWithdraw(2, 120, 20), true);
+      assert.equal(await assertWithdraw(3, 110, 10), true);
+      assert.equal(await assertWithdraw(4, 100, 0), true);
+    });
+
+    it("cancels", async function() {
+      await sale.cancel({from:owner});
+      assert.equal((await sale.end.call()), true);
+      assert.equal((await sale.stage.call()), 2);
+      assert.equal(await assertWithdraw(1, 120, 120), true);
+      assert.equal(await assertWithdraw(2, 120, 120), true);
+      assert.equal(await assertWithdraw(3, 110, 110), true);
+      assert.equal(await assertWithdraw(4, 100, 100), true);
+    });
+
+    // TODO additional tests.
+    // Non admin cannot finalise
+    // Non admin cannot cancel
+    // Cannot buy after ended.
+    // Cannot withdraw twice.
+  })
 });
+

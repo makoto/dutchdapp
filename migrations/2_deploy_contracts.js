97f4292331a96abdaf3875e8793522760bb51fb1
Makoto <inouemak@googlemail.com>
2018-05-23 20:17:21 +0100

    "The" moon

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var CrowdSale = artifacts.require("CrowdSale");
 let startPrice = web3.toWei(120, 'ether');
 let targetPrice = web3.toWei(60, 'ether');
 module.exports = function(deployer) {
-  deployer.deploy(CrowdSale, 0, '@pitbull charity gig', 'Moon', '@pitbull', Date.now(), Date.now(10000), startPrice, targetPrice, 10, 4);
+  deployer.deploy(CrowdSale, 0, '@pitbull charity gig', 'the Moon', '@pitbull', Date.now(), Date.now(10000), startPrice, targetPrice, 10, 4);
 };
